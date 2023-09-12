75bf9a39d017bdad29324cfb01ffdcd723a3f225
jacob-acton <jacton@2ndwatch.com>
2023-09-12 15:10:32 -0500

    removing unused code

--- example/vwan.tf
+++ example/vwan.tf
@@ -13,30 +13,3 @@ module "vwan" {
     environment = "demo"
   }
 }
-
-module "s2s_law" {
-  source = "../terraform-modules/log-analytics-workspace"
-
-  resource_group_name = "demo-rg"
-  name                = "demo-s2s-law"
-
-  tags = {
-    environment = "demo"
-  }
-}
-
-module "vwan_s2s" {
-  source = "../terraform-modules/vwan-s2s"
-
-  resource_group_name        = "demo-rg"
-  virtual_hub_id             = module.vwan.vwan_hub_id
-  virtual_wan_id             = module.vwan.vwan_id
-  address_cidrs              = ["10.0.0.0/16"]
-  ip_address                 = "1.1.1.1"
-  speed_in_mbps              = 50
-  log_analytics_workspace_id = module.s2s_law.id
-
-  tags = {
-    environment = "demo"
-  }
-}
